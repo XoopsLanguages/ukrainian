3bd7e1a17bb263bbd43a1ae9787c4427808a800e
mambax7 <mambax7@gmail.com>
2016-08-09 10:00:40 -0400

    About module

---
+++ modules/about/ukrainian/admin.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * About
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright      The XOOPS Co.Ltd. http://www.xoops.com.cn
+ * @copyright      XOOPS Project (http://xoops.org)
+ * @license        GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package        about
+ * @since          1.0.0
+ * @author         Mengjue Shao <magic.shao@gmail.com>
+ * @author         Susheng Yang <ezskyyoung@gmail.com>
+ * @version        $Id: admin.php 1 2010-2-9 ezsky$
+ */
+
+// _LANGCODE: zh-CN
+// _CHARSET : utf-8
+// Author: magic.shao
+
+//admin base
+define('_AM_ABOUT_SAVEDSUCCESS', 'Збережено успішно!');
+define('_AM_ABOUT_DELETESUCCESS', 'Видалено успішно!');
+define('_AM_ABOUT_RUSUREDEL', 'Ви впевнені, що хочете видалили?');
+define('_AM_ABOUT_TIME', 'Дата');
+define('_AM_ABOUT_ACTION', 'Дія');
+
+//admin.PAGE
+define('_AM_ABOUT_PAGE_ORDER_ERROR', 'Зовнішнє посилання на цю сторінку не може бути домашньою сторінкою.');
+define('_AM_ABOUT_PAGE_INSERT', 'Додати сторінку');
+
+//form.page
+define('_AM_ABOUT_EDIT', 'Редагувати');
+define('_AM_ABOUT_PAGE_TITLE', 'Назва');
+define('_AM_ABOUT_PAGE_MENU_LIST', 'Відобразити у меню?');
+define('_AM_ABOUT_PAGE_MENU_TITLE', 'Назва меню');
+define('_AM_ABOUT_PAGE_HIGHER', 'Домашня сторінка');
+define('_AM_ABOUT_PAGE_LINK_MENU', '( Якщо цитата назви вільна )');
+define('_AM_ABOUT_PAGE_TEXT', 'Зміст');
+define('_AM_ABOUT_CATEGORY_SELECT', 'Обрати категорію');
+define('_AM_ABOUT_PAGE_LINK_TEXT', 'Зовнішні посилання');
+define('_AM_ABOUT_PAGE_LINK_BLANK', 'Відобразити у новому вікні');
+define('_AM_ABOUT_PAGE_STATUS', 'Опублікувати?');
+define('_AM_ABOUT_PAGE_DARFT', 'Чорнетка');
+define('_AM_ABOUT_PAGE_SUB', 'Опубліковано');
+define('_AM_ABOUT_TEMPLATE_SELECT', 'Шаблон');
+define('_AM_ABOUT_PAGE_IMAGE', 'Головне зображення');
+
+//about_admin_page.tpl
+define('_AM_ABOUT_INSERT_LINK', 'Додати зовнішнє посилання');
+define('_AM_ABOUT_PAGE_MENU_ORDER', 'Сортування');
+define('_AM_ABOUT_PAGE_CUS_INDEX', 'Перелік');
+define('_AM_ABOUT_PAGE_TEMPLATE', 'Шаблон');
+define('_AM_ABOUT_PAGE_TYPE', 'Тип');
+define('_AM_ABOUT_PAGE_STATUS_LIST', 'Статус');
+define('_AM_ABOUT_PAGE_MENU', 'Меню');
+define('_AM_ABOUT_PAGE_AUTHOR', 'Редактор');
---
+++ modules/about/ukrainian/modinfo.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * About
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright      The XOOPS Co.Ltd. http://www.xoops.com.cn
+ * @copyright      XOOPS Project (http://xoops.org)
+ * @license        GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package        about
+ * @since          1.0.0
+ * @author         Mengjue Shao <magic.shao@gmail.com>
+ * @author         Susheng Yang <ezskyyoung@gmail.com>
+ * @version        $Id: modinfo.php 1 2010-2-9 ezsky$
+ */
+
+// _LANGCODE: zh-CN
+// _CHARSET : utf-8
+// Translator: ezsky, http://www.ezsky.org
+
+//xoops_version  add   menu
+define('_MI_ABOUT_NAME', 'Про нас');
+define('_MI_ABOUT_DESC', "Зовнішній 'Про нас' модуль интернет сторінки для XOOPS");
+define('_MI_ABOUT_PAGE', 'Сторінка');
+define('_MI_ABOUT_ABOUTUS', 'Про нас');
+
+define('_MI_ABOUT_CONFIG_LIST', 'Відобразити');
+define('_MI_ABOUT_CONFIG_LIST_CATEGORY', 'Категорія');
+define('_MI_ABOUT_CONFIG_LIST_PAGE', 'Сторінка');
+define('_MI_ABOUT_CONFIG_STR_EREG', 'Резюме статті лімітовано');
+
+define('_MI_ABOUT_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('_MI_ABOUT_HELP_HEADER', __DIR__ . '/help/helpheader.html');
+define('_MI_ABOUT_BACK_2_ADMIN', 'Повернутися до Админістрування ');
+
+//define('_MI_ABOUT_HELP_DIR', __DIR__);
+
+//help
+define('_MI_ABOUT_HELP_OVERVIEW', 'Огляд');
+
+define('_MI_ABOUT_EDITOR', 'Обрати редактор');
+define('_MI_ABOUT_EDITOR_DESC', 'Ви можете обрати тут ваш Текстовий редактор');
